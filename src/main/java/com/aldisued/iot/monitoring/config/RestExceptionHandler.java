e225f34b7bed59c2799c5655f68c66896f833627
István Jakab <github@code.istvanjakab.dev>
2026-09-11 22:36:23 +0200

    feat: create NotFoundException and its REST handler

--- src/main/java/com/aldisued/iot/monitoring/config/RestExceptionHandler.java
+++ src/main/java/com/aldisued/iot/monitoring/config/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.aldisued.iot.monitoring.config;
 
+import com.aldisued.iot.monitoring.exception.NotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
@@ -12,6 +13,11 @@ public class RestExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ErrorResponse handleDataIntegrityViolation(DataIntegrityViolationException ex) {
         return ErrorResponse.builder(ex, HttpStatus.CONFLICT, ex.getMessage()).build();
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorResponse handleNotFoundException(NotFoundException ex) {
+        return ErrorResponse.builder(ex, HttpStatus.NOT_FOUND, ex.getMessage()).build();
     }
 
 }
