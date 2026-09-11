8c72bc5e6081879a1e2e09dece01a42b8898aa85
István Jakab <github@code.istvanjakab.dev>
2026-09-11 22:45:12 +0200

    test: add test case that properly checks missing alerts for an existing sensor
    - Add new sensor insert statement to the SQL script.
    - Create test method to verify that HTTP 404 is thrown when no alerts are found for an existing sensor.
    - Note: Pre-existing test method's name was not modified.

--- src/test/java/com/aldisued/iot/monitoring/tasks/Task5Tests.java
+++ src/test/java/com/aldisued/iot/monitoring/tasks/Task5Tests.java
@@ -13,11 +13,15 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+//TODO rename script file to task-5-test-data
 @Sql(scripts = "/sql/task-4-test-data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_CLASS)
 public class Task5Tests extends IntegrationTestBase {
 
   private static final UUID SENSOR_ID = UUID.fromString(
       "e3242ea2-0514-46d3-aad8-b2012980c41c");
+
+  private static final UUID SENSOR_ID_NO_ALERTS = UUID.fromString(
+      "e3242ea2-0514-46d3-aad8-b2012980c41d");
 
   private static final String BASE_ENDPOINT = "/alerts";
 
@@ -38,11 +42,20 @@ public class Task5Tests extends IntegrationTestBase {
         .andExpect(status().isBadRequest());
   }
 
+  // not found sensor != found sensor that has no alerts. (I won't modify the pre-existing method name.)
   @Test
   public void verifyMissingAlertHandling() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.get(BASE_ENDPOINT + "/latest")
             .queryParam("sensorId", UUID.randomUUID().toString()))
         .andExpect(status().isNotFound());
+  }
+
+  /// Verify missing alerts for existing sensor.
+  @Test
+  public void verifyMissingAlertHandling2() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get(BASE_ENDPOINT + "/latest")
+                    .queryParam("sensorId", SENSOR_ID_NO_ALERTS.toString()))
+            .andExpect(status().isNotFound());
   }
 
 
